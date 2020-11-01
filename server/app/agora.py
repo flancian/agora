20f3c7b788bc5a27ba07b037159b0495983ff464
Flancian <0@flancia.org>
2020-11-01 17:51:10 +0100

    Add fake db, implement listing notes.

--- server/app/agora.py
+++ server/app/agora.py
@@ -1,9 +1,7 @@
 from flask import Blueprint, url_for, render_template, current_app
 from markupsafe import escape
-import glob
+from . import db
 bp = Blueprint('agora', __name__)
-
-path='../../'
 
 @bp.route('/')
 def index():
@@ -14,11 +12,9 @@ def help():
     current_app.logger.warning('Not implemented.')
     return 'If I had implemented help already, here you\'d see documentation on all URL endpoints. For now, please refer to the <a href="https://flancia.org/go/agora">code</a>.'
 
-@bp.route('/ls')
-def ls():
-    current_app.logger.warning('Not implemented.')
-    posts = [f for f in glob.glob(path + '**/*.md', recursive=True)]
-    return str(posts)
+@bp.route('/notes')
+def notes():
+    return render_template('notes.html', notes=db.all_notes())
 
 @bp.route('/user/<username>')
 def user(username):
