49d8cad603abcf0713994dd8289c2353710ab7a8
Flancian <0@flancia.org>
2022-07-09 17:15:17 +0200

    Add update.sh to automate integration a bit.

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Runs what I run in the anagora.org environment (Debian Linux).
+#
+# This script assumes a lot :)
+#
+# Essentially:
+#
+#   - This repository is checked out in $HOME/agora and it contains a valid sources.yaml.
+#   - There is a $HOME/agora-server directory with virtualenvs set up, e.g. if you cd there and run ./update-dev.sh the server starts.
+#   - There is a $USER/agora-bridge directory with virtualenvs set up, e.g. if you cd there and run ./update-dev.sh the bridge starts.
+#
+# This should all work out of the box if you run an Agora on [[docker]]/[[podman]]; so see instructions in Dockerfile if you haven't set up agora-server, agora-bridge yet.
+
+MODE=${1:-dev}
+
+cd ../agora-server && ./update-${MODE}.sh 2>&1 > agora-server-update.log &
+cd ../agora-bridge && ./update-${MODE}.sh 2>&1 > agora-bridge-update.log &
+
+wait -n
+exit $?
