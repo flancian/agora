58064b8bce1fc4dcbc7f8a215251838259e34f16
Flancian <0@flancia.org>
2024-11-20 00:01:00 +0100

    Add hacky, but useful to me, run-dev.sh script.

--- run-dev.sh
+++ run-dev.sh
@@ -2,4 +2,16 @@
 #
 # Caveats in run.sh apply :)
 
-./run.sh dev
+# ./run.sh dev
+
+#!/bin/bash
+# hack hack hack
+#
+#nohup deno task watch-web &
+#nohup deno task watch-server ~/garden &
+#nohup deno task watch-plugs
+
+echo "This assumes you are running tmux... and a lot more :)"
+tmux split-window -h "cd ~/agora-server && ./run-localdev.sh"
+tmux split-window -h "cd ~/agora-server && npx esbuild app/js-src/index.ts --bundle --outfile=app/static/js/index.js --watch"
+tmux split-window -h "cd ~/agora-bridge && ./run-dev.sh"
